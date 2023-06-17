81789caa8d8c68c15d81ff655a1a88283907bfe8
Serhii Kondratenko <k.serjio@ukr.net>
2023-06-18 02:15:55 +0300

    Finish 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,32 @@
 
-const iframe = document.querySelector('iframe');
-const iframePlayer = new Vimeo.Player(iframe);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+    const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+    
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+    
+    const onPlay = function(data) {
+        // data is an object containing properties specific to that event
+
+        localStorage.setItem("videoplayer-current-time", data.seconds);
+        // console.log(data.seconds);
+    };
+
+
+    player.on('timeupdate', throttle(onPlay, 1000));
+    
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
 
+const formEl = document.querySelector(".feedback-form");
+
+// const emailInputEl = document.querySelector(".email");;
+// const textareaInputEl = document.querySelector(".message");;
+
+const formData = {};
+
+formEl.addEventListener("submit", handldeFormSubmit);
+
+formEl.addEventListener("input", (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    formData[event.target.name] = event.target.value;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData)); 
+
+} );
+
+
+
+
+    const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+    if (savedMessage) {
+        // console.log(savedMessage)
+         
+    }
+
+
+function handldeFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(formData)
+
+    event.target.reset();
+};
+
+
+
