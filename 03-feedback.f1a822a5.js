d6dab24c418d9acd8ba4cd8dfb856c47a6b88918
Serjio2 <130094175+Serjio2@users.noreply.github.com>
2023-06-18 09:04:33 +0000

    Deploying to gh-pages from @ Serjio2/goit-js-hw-08@b7be2c2a9f2f7766e9ca60039bf6372a8765bebd 🚀

--- 03-feedback.f1a822a5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),t={};e.addEventListener("submit",(function(e){e.preventDefault(),console.log(t),e.target.reset()})),e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}));JSON.parse(localStorage.getItem("feedback-form-state"))}();
-//# sourceMappingURL=03-feedback.f1a822a5.js.map
