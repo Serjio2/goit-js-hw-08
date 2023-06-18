3c629c2c721236407965440f9a21437c556f2777
Serhii Kondratenko <k.serjio@ukr.net>
2023-06-18 12:17:23 +0300

    ?++

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,7 +3,9 @@
 
 // const emailInputEl = document.querySelector(".email");;
 // const textareaInputEl = document.querySelector(".message");;
+console.log(formEl)
 
+<<<<<<< Updated upstream
 // const formData = {};
 
 // formEl.addEventListener("submit", handldeFormSubmit);
@@ -16,10 +18,52 @@
 //     localStorage.setItem("feedback-form-state", JSON.stringify(formData)); 
 
 // } );
+=======
+
+// formEl.addEventListener("input", inputFormEl)
+
+// // formEl.addEventListener("submit", handldeFormSubmit);
+
+// // savedMessageUsers();
+
+// const formData = {};
+
+
+// function handldeFormSubmit(event) {
+//     // event.preventDefault();
+
+//     // console.log(formData)
+
+//     // event.target.reset();
+// };
+
+
+// function inputFormEl(event) {
+//     // formData[event.target.name] = event.target.value;
+//     // console.log(event.target.name)
+//     // console.log(event.target.value)
+
+//     // localStorage.setItem("feedback-form-state", JSON.stringify(formData)); 
+// };
+
+
+// function savedMessageUsers() {
+
+//     // const savedMessage = localStorage.getItem("feedback-form-state");
+
+    
+//     // if (savedMessage) {
+        
+//     //     console.log(savedMessage)
+//     // }
+   
+// };
+>>>>>>> Stashed changes
 
 
 
 
+<<<<<<< Updated upstream
 //     const savedMessage = JSON.parse(localStorage.getItem("feedback-form-state"));
 
 //     if (savedMessage) {
@@ -37,4 +81,6 @@
 // };
 
 
+=======
+>>>>>>> Stashed changes
 
